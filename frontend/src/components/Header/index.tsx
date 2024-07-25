dac178f8005679c2a9babebe09992d1e1b265ee2
developer-thomas <andepipous@gmail.com>
2024-07-25 00:10:24 -0400

    style: create login icon and add hover effect to login button

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import logo from '../../assets/images/logo.png'
+import logo from "../../assets/images/logo.png";
 
-import closeButton from '../../assets/images/close-button.png'
+import closeButton from "../../assets/images/close-button.png";
 
-
-import * as S from './styles'
+import * as S from "./styles";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <S.Header>
       <S.ContentContainer>
-        <S.Logo src={logo} alt='Logo'/>
+        <S.Logo src={logo} alt="Logo" />
         <S.NavBar>
-          <a href='#contato'>Contato</a>
-          <a href='#trilhas'>Trilhas</a>
-          <a href='#login'>
+          <a href="#contato">Contato</a>
+          <a href="#trilhas">Trilhas</a>
+          <a href="#login">
+            <S.LoginIcon></S.LoginIcon>
             Login
           </a>
         </S.NavBar>
-        { isMenuOpen ? (
-          <S.CloseButton src={closeButton} onClick={() => setIsMenuOpen(false)}/>
-        ) : 
+        {isMenuOpen ? (
+          <S.CloseButton src={closeButton} onClick={() => setIsMenuOpen(false)} />
+        ) : (
           <S.Hamburguer onClick={() => setIsMenuOpen(true)}>
             <span />
             <span />
             <span />
             <span />
           </S.Hamburguer>
-        }        
+        )}
       </S.ContentContainer>
-      <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
-        <a href='#participeagora'>Participe agora</a>
-        <a href='#home'>Home</a>
-        <a href='#trilhas'>Trilhas</a>
-        <a href='#contato'>Contato</a>
-      </ S.NavMobile>
+      <S.NavMobile className={isMenuOpen ? "is-open" : ""}>
+        <a href="#participeagora">Participe agora</a>
+        <a href="#home">Home</a>
+        <a href="#trilhas">Trilhas</a>
+        <a href="#contato">Contato</a>
+      </S.NavMobile>
     </S.Header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
