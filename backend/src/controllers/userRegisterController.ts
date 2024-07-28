4cd1f559e30637c53ed8849b1c6d5e7c795e0686
Alexandre Cabral <alexandre_c.m@hotmail.com>
2024-07-28 12:29:42 -0300

    feat: automated the creation of the database and table, and updated the user creation

--- backend/src/controllers/userRegisterController.ts
+++ backend/src/controllers/userRegisterController.ts
@@ -7,19 +7,25 @@ const registerUser = async(req:Request, res:Response) => {
   const { name, email, phone, password } = req.body;
   
   const user: User = { name, email, phone, password };
+
   if (!name || !email || !phone || !password) {
     return res.status(400).json({ error: 'All fields are mandatory.' });
   }
+
   try {
     const emailExists = await checkIfEmailExists(email);
     
     if (emailExists) {
       return res.status(409).json({ message: 'User already registered' });
     }
+    
     const newUser = await createUser(user);
+    
     res.status(201).json(newUser);
-  } catch (error) {
+
+  } catch (error) {  
     res.status(500).json({ error: 'Failed to create user' });
   };
+
 };
 export { registerUser };
