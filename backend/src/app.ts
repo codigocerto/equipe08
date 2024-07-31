3c4a258a730bd96d5aafbf2e6e2196dc09199469
Alexandre Cabral <alexandre_c.m@hotmail.com>
2024-07-31 09:08:26 -0300

    feat(login route and controller): add login router and controller
    adicionei a rota de login do arquivo app.ts, assim como no routes/login.route.ts, também adicionei o
    controller do login no arquivo controller/login.controller.ts e a tipagem  do jwtPayload na pasta
    @types

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes';
-//import { loginRoute } from "./routes/login.route";
+import { loginRoute } from "./routes/login.route";
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-//app.use('/login', loginRoute)
+app.use('/login', loginRoute)
 
 const PORT = process.env.PORT;
 
