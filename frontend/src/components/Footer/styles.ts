6e3952e2986f66023c3c7222604dc961535127bb
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2024-09-18 20:08:38 -0300

    refactor(git): update footer appearance

--- frontend/src/components/Footer/styles.ts
+++ frontend/src/components/Footer/styles.ts
@@ -10,11 +10,15 @@ import { breakpoints } from '../../styles'
 
 export const Footer = styled.footer`
   background-color: ${colors.black};
-  padding: ${remCalc(32)} 0 ${remCalc(16)};
+  padding: ${remCalc(70)} ${remCalc(170)} 0 ${remCalc(170)};
 
-  @media (max-width: ${breakpoints.desktop}) {
-    padding: ${remCalc(16)} ${remCalc(32)} ${remCalc(12)};
-  }
+  @media (max-width: ${breakpoints.tablet}) {
+    padding: ${remCalc(25)} ${remCalc(40)} 0 ${remCalc(40)};
+    }
+
+  @media (max-width: ${breakpoints.mobile}) {
+    padding: ${remCalc(25)} ${remCalc(10)} 0 ${remCalc(10)};
+    }
 `
 
 export const MainContainer = styled.div`
@@ -27,8 +31,10 @@ export const MainContainer = styled.div`
 
 export const ContentContainer = styled.div`
   display: flex;
-  height: ${remCalc(224)};
-  padding: ${remCalc(16)} 0;
+  justify-content: space-between;
+  align-items: center;
+  height: ${remCalc(125)};
+  padding: 0 ${remCalc(8)};
   border-top: ${remCalc(2)} solid ${colors.gray3};
   border-bottom: ${remCalc(2)} solid ${colors.gray3};
 
@@ -36,102 +42,15 @@ export const ContentContainer = styled.div`
     height:  ${remCalc(72)};
     width: ${remCalc(100)};
 
-    @media (max-width: ${breakpoints.desktop}) {
-      height:  ${remCalc(56)};
-      width: ${remCalc(84)};
-      margin-bottom: ${remCalc(16)};
+    @media (max-width: ${breakpoints.tablet}) {
+      height: ${remCalc(50)};
+      width: ${remCalc(70)};
     }
   }
 
-  @media (max-width: ${breakpoints.desktop}) {
-    position: relative;
-    flex-direction: column;
-    padding: 0;
-    border-top: none;
-    border-bottom: none;
-    height: auto;
-  }
-`
-
-export const ContactColumn = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-  color: ${colors.gray2};
-  margin-top: ${remCalc(22)};
-  margin-left: ${remCalc(200)};
-
-  h3 {
-    text-transform: uppercase;
-    font-weight: 500;
-    font-size: ${remCalc(20)};
-    line-height: ${remCalc(32)};
-
-    @media (max-width: ${breakpoints.desktop}) {
-      font-size: ${remCalc(18)};
+  @media (max-width: ${breakpoints.tablet}) {
+    height: ${remCalc(80)};
     }
-  }
-
-  li {
-    font-weight: 400;
-    font-size: ${remCalc(14)};
-    line-height: ${remCalc(36)};
-
-    @media (max-width: ${breakpoints.desktop}) {
-      font-weight: 500;
-      font-size: ${remCalc(12)};
-      line-height: ${remCalc(32)};
-    }
-  }
-
-  @media (max-width: ${breakpoints.desktop}) {
-    margin-top: 0;
-    margin-left: 0;
-    padding: ${remCalc(8)} 0 ${remCalc(8)};
-    border-top: ${remCalc(1)} solid ${colors.gray3};
-    border-bottom: ${remCalc(1)} solid ${colors.gray3};
-  }
-`
-
-export const ContactColumnTitle = styled(Typography)`
-  text-transform: uppercase;
-  font-weight: 500;
-  font-size: ${remCalc(20)};
-  line-height: ${remCalc(32)};
-
-  @media (max-width: ${breakpoints.desktop}) {
-    font-size: ${remCalc(18)};
-  }
-`
-
-export const Email = styled(Typography)`
-  font-weight: 400;
-  font-size: ${remCalc(14)};
-  line-height: ${remCalc(36)};
-
-  @media (max-width: ${breakpoints.desktop}) {
-    font-weight: 500;
-    font-size: ${remCalc(12)};
-    line-height: ${remCalc(32)};
-  }
-`
-
-export const InfoColumn = styled(ContactColumn)`
-  display: revert;
-
-  h3 {
-    margin-bottom: ${remCalc(30)};
-
-    @media (max-width: ${breakpoints.desktop}) {
-      margin-bottom: ${remCalc(4)};
-    }
-  }
-
-  @media (max-width: ${breakpoints.desktop}) {
-    border-top: none;
-    border-bottom: ${remCalc(1)} solid ${colors.gray3};
-    padding-bottom: ${remCalc(8)};
-  }
 `
 
 export const FooterLink = styled(Link)`
@@ -145,28 +64,29 @@ export const FooterLink = styled(Link)`
 
 export const SocialMedia = styled.ul`
   display: flex;
+  gap: ${remCalc(50)};
 
   i {
     color: ${colors.gray2};
-    font-size: ${remCalc(30)};
-    margin-right: ${remCalc(32)};
+    font-size: ${remCalc(50)};
     transition: color .5s ease-out;
 
     &:hover {
       color: ${colors.red};
     }
 
-    @media (max-width: ${breakpoints.desktop}) {
-      font-size: ${remCalc(24)};
-      margin-right: ${remCalc(12)};
+    @media (max-width: ${breakpoints.tablet}) {
+      font-size: ${remCalc(35)};
     }
   }
 
-  @media (max-width: ${breakpoints.desktop}) {
-    position: absolute;
-    top: 5%;
-    right: ${remCalc(4)};
-  }
+  @media (max-width: ${breakpoints.tablet}) {
+      gap: ${remCalc(20)};
+    }
+
+  @media (max-width: ${breakpoints.mobile}) {
+      gap: ${remCalc(12)};
+    }
 `
 
 export const Copyright = styled(Typography)`
@@ -174,11 +94,14 @@ export const Copyright = styled(Typography)`
   font-weight: 400;
   font-size: ${remCalc(14)};
   line-height: ${remCalc(19.36)};
-  margin-top: ${remCalc(16)};
+  padding: ${remCalc(28)} 0;
 
-  @media (max-width: ${breakpoints.desktop}) {
+  @media (max-width: ${breakpoints.tablet}) {
+    padding: ${remCalc(12)} 0;
+    text-align: center;
+  }
+
+  @media (max-width: ${breakpoints.mobile}) {
     font-size: ${remCalc(10)};
-    line-height: ${remCalc(12)};
-    margin-top: ${remCalc(15)};
   }
 `
