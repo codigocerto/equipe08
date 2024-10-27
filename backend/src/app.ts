feadea419ba596a5c711c6c4f4f2158f7c7874ab
Alexandre Cabral <alexandre_c.m@hotmail.com>
2024-10-27 09:28:18 -0300

    feat(add cors and error handling for password): adiciona o CORS e o tratamento de erro para senha
    adicionado o CORS para liberar as chamadas de requisições e também adiciona mais um tratamento de
    erro para a senha caso não seja informado uma senha dentro das regras de validação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes';
 import { loginRoute } from "./routes/login.route";
-
+import cors from "cors"
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
@@ -13,5 +15,5 @@ app.use('/login', loginRoute)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
